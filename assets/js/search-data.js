92cc4c71b7a8a7b1c53dc75a72881c48eed8ecba
ibcmrocha <75364593+ibcmrocha@users.noreply.github.com>
2025-10-25 14:08:50 +0000

    Deploying to gh-pages from @ ibcmrocha/ibcmrocha.github.io@f5bd98becac9f619c5b0b3fd1d712a1189ab6008 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/group/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-publications",
           title: "publications",
           description: "",
